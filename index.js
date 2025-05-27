2af3384ce53c297c86be5f754d9b65ba426467ce
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-05-27 19:35:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use('/mx',async (req, res, next) => {
 res.sendFile(__path + '/mx.html')
 })
 app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/MX-2.0.html')
+res.sendFile(__path + '/meka.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`YoutTube: mxgamecoder\nTelegram: mxgamecoderr\nGitHub: @themxgamecoder
+    console.log(`YoutTube: mxgamecoder\nTelegram: mxgamecoderr\nGitHub: https://t.me/mxgamecoderr
 \nInstsgram: themxgamecoder\nPLEASE STAR OUR REPO\nMX-GΔMΞCØDΞR
  LOVE YOU GUYS
 \n\nServer running on http://localhost:` + PORT)
