71b92994e35ce59246589f1aa98e4cb8ad3c23ae
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-06-13 14:35:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const path = require("path");
 const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
+let code = require('./mx');
 require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Routes
-const mxRoutes = require('./mx');
-
-// ⚙️ Middlewares
+app.use('/code', code);
+app.use('/mx',async (req, res, next) => {
+res.sendFile(__path + '/mx.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/meka.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// 👇 Mount your routes FIRST
-app.use('/', mxRoutes); // so /creds and /?number=... works!
-
-// 🌐 Serve fallback HTML for unmatched routes
-app.get('/mx', (req, res) => {
-  res.sendFile(path.join(__dirname, 'mx.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'meka.html'));
-});
-
-// 🚀 Start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-  console.log(`MX-GΔMΞCØDΞR | YouTube: @mxgamecoder`);
-});
+    console.log(`YoutTube: mxgamecoder\nTelegram: mxgamecoderr\nGitHub: https://t.me/mxgamecoderr
+\nInstsgram: themxgamecoder\nPLEASE STAR OUR REPO\nMX-GΔMΞCØDΞR
+ LOVE YOU GUYS
+\n\nServer running on http://localhost:` + PORT)
+})
+
+module.exports = app
